fe6b9eb0fdd5787cf0ff6d5f5674645c5226202f
MikageBen <ben69@uw.edu>
2025-03-03 21:46:28 +0900

    Full Pokemon-style update

--- game.js
+++ game.js
@@ -2,16 +2,25 @@ const config = {
     type: Phaser.AUTO,
     width: 800,
     height: 600,
+    parent: 'game-container',
     scene: { preload, create, update }
   };
   
   const game = new Phaser.Game(config);
-  let player, mapTiles, beasts = [], ui;
+  let player, mapTiles, currentBeastSprite, worldMap = {}, encounterCooldown = 0;
   
   function preload() {
     this.load.image('forest', 'https://opengameart.org/sites/default/files/forest.png');
     this.load.image('mountain', 'https://opengameart.org/sites/default/files/mountain.png');
     this.load.image('swamp', 'https://opengameart.org/sites/default/files/swamp.png');
+    this.load.image('monster_forest', 'https://opengameart.org/sites/default/files/monster_forest.png');
+    this.load.image('monster_mountain', 'https://opengameart.org/sites/default/files/monster_mountain.png');
+    this.load.image('monster_swamp', 'https://opengameart.org/sites/default/files/monster_swamp.png');
+    this.load.image('monster_forest_shiny', 'https://opengameart.org/sites/default/files/monster_forest_shiny.png');
+    this.load.image('monster_mountain_shiny', 'https://opengameart.org/sites/default/files/monster_mountain_shiny.png');
+    this.load.image('monster_swamp_shiny', 'https://opengameart.org/sites/default/files/monster_swamp_shiny.png');
+    this.load.image('hat', 'https://opengameart.org/sites/default/files/hat.png');
+    this.load.image('vest', 'https://opengameart.org/sites/default/files/vest.png');
   }
   
   function create() {
@@ -19,44 +28,89 @@ const config = {
     for (let x = 0; x < 10; x++) {
       for (let y = 0; y < 10; y++) {
         let tileType = ['forest', 'mountain', 'swamp'][Math.floor(Math.random() * 3)];
-        let tile = this.add.sprite(x * 64, y * 64, tileType).setInteractive();
-        tile.on('pointerdown', () => movePlayer(x, y));
+        worldMap[`${x},${y}`] = tileType;
+        let tile = this.add.sprite(x * 64, y * 64, tileType);
         mapTiles.add(tile);
       }
     }
   
-    player = { x: 5, y: 5, strength: 10, charm: 10, roster: [], fame: localStorage.getItem('fame') || 0 };
-    this.add.rectangle(player.x * 64, player.y * 64, 32, 32, 0x00ff00);
+    player = { x: 5, y: 5, strength: 10, charm: 10, roster: [], fame: localStorage.getItem('fame') || 0, cosmetic: localStorage.getItem('cosmetic') || '' };
+    this.playerSprite = this.add.sprite(player.x * 64, player.y * 64, player.cosmetic || 'hat').setScale(0.5);
+    this.playerSprite.depth = 1;
   
-    ui = {
-      fightBtn: this.add.text(650, 500, 'Fight', { color: '#fff', backgroundColor: '#333', padding: 5 }).setInteractive(),
-      tameBtn: this.add.text(700, 500, 'Tame', { color: '#fff', backgroundColor: '#333', padding: 5 }).setInteractive()
-    };
-    ui.fightBtn.on('pointerdown', () => resolveEncounter('fight'));
-    ui.tameBtn.on('pointerdown', () => resolveEncounter('tame'));
+    document.getElementById('fame-value').innerText = player.fame;
+    fetch('https://beastbinders-7555f963f9c1.herokuapp.com/get-roster?playerId=player123')
+      .then(res => res.json())
+      .then(data => {
+        player.roster = data;
+        updateRosterUI();
+      });
+  
+    this.cursors = this.input.keyboard.createCursorKeys();
+    this.keys = this.input.keyboard.addKeys('W,A,S,D');
   }
   
   function update() {
-    if (player.x > 9 || player.y > 9) expandMap();
+    if (this.cursors.left.isDown || this.keys.A.isDown) {
+      movePlayer(player.x - 1, player.y);
+    } else if (this.cursors.right.isDown || this.keys.D.isDown) {
+      movePlayer(player.x + 1, player.y);
+    } else if (this.cursors.up.isDown || this.keys.W.isDown) {
+      movePlayer(player.x, player.y - 1);
+    } else if (this.cursors.down.isDown || this.keys.S.isDown) {
+      movePlayer(player.x, player.y + 1);
+    }
+  
+    if (encounterCooldown <= 0 && Math.random() < 0.05) {
+      resolveEncounter('prompt');
+      encounterCooldown = 60;
+    } else if (encounterCooldown > 0) {
+      encounterCooldown--;
+    }
   }
   
   function movePlayer(x, y) {
+    expandMap(x, y);
     player.x = x;
     player.y = y;
-    game.scene.scenes[0].children.list.find(c => c.type === 'Rectangle').setPosition(x * 64, y * 64);
+    this.playerSprite.setPosition(x * 64, y * 64);
+  }
+  
+  function expandMap(x, y) {
+    if (!worldMap[`${x},${y}`]) {
+      const newBiome = ['forest', 'mountain', 'swamp'][Math.floor(Math.random() * 3)];
+      worldMap[`${x},${y}`] = newBiome;
+      const tile = this.add.sprite(x * 64, y * 64, newBiome);
+      mapTiles.add(tile);
+    }
   }
   
   async function resolveEncounter(action) {
-    const tileBiome = mapTiles.getChildren()[player.y * 10 + player.x].texture.key;
+    const tileBiome = worldMap[`${player.x},${player.y}`] || 'forest'; // Fallback
     const response = await fetch(`https://beastbinders-7555f963f9c1.herokuapp.com/generate-beast?biome=${tileBiome}`);
     const beast = await response.json();
+  
+    if (currentBeastSprite) currentBeastSprite.destroy();
+    currentBeastSprite = game.scene.scenes[0].add.sprite(400, 300, `monster_${beast.visual.split('/').pop().split('.')[0]}`).setScale(2);
+  
+    if (action === 'prompt') {
+      const choice = prompt(`A wild ${beast.name} (${beast.traits.color}, ${beast.traits.size}) appears! Fight or Tame? (f/t)`);
+      if (choice.toLowerCase() === 'f') action = 'fight';
+      else if (choice.toLowerCase() === 't') action = 'tame';
+      else {
+        currentBeastSprite.destroy();
+        return;
+      }
+    }
   
     if (action === 'fight') {
       if (player.strength > Math.random() * 20) {
         alert(`Defeated ${beast.name}!`);
         player.strength += 1;
+        currentBeastSprite.destroy();
       } else {
         alert(`${beast.name} escaped!`);
+        currentBeastSprite.destroy();
       }
     } else if (action === 'tame') {
       if (player.charm > Math.random() * 20) {
@@ -65,8 +119,11 @@ const config = {
         player.charm += 1;
         generateBondSeal(beast);
         saveRoster(beast);
+        updateRosterUI();
+        currentBeastSprite.destroy();
       } else {
         alert(`${beast.name} slipped away!`);
+        currentBeastSprite.destroy();
       }
     }
   }
@@ -75,31 +132,34 @@ const config = {
     const fameBoost = beast.rarity === 'shiny' ? 20 : (beast.rarity === 'rare' ? 10 : 2);
     player.fame = parseInt(player.fame) + fameBoost;
     localStorage.setItem('fame', player.fame);
+    document.getElementById('fame-value').innerText = player.fame;
     const tweet = `Tamed ${beast.name} (${beast.personality})${beast.rarity === 'shiny' ? ' - Shiny!' : ''} in Beast Binders! #BeastBinders`;
     alert(`Copy to X: ${tweet}`);
   }
   
   function saveRoster(beast) {
-    fetch('https://beastbinders-7555f963f9c1.herokuapp.com/save-roster', {
+    fetch('https://beastbinders-7555f963f9c1.com/save-roster', {
       method: 'POST',
       body: JSON.stringify({ playerId: 'player123', beast }),
       headers: { 'Content-Type': 'application/json' }
     });
   }
   
-  function expandMap() {
-    const newBiome = ['forest', 'mountain', 'swamp'][Math.floor(Math.random() * 3)];
-    const tile = game.scene.scenes[0].add.sprite((player.x + 1) * 64, player.y * 64, newBiome).setInteractive();
-    tile.on('pointerdown', () => movePlayer(player.x + 1, player.y));
-    mapTiles.add(tile);
+  function updateRosterUI() {
+    document.getElementById('roster-list').innerHTML = player.roster.map(b => 
+      `<li class="${b.rarity === 'shiny' ? 'shiny' : ''}">${b.name} (${b.personality})${b.rarity === 'shiny' ? ' - Shiny' : ''}</li>`
+    ).join('');
   }
   
   function claimCosmetic(item) {
     if (player.fame >= 50) {
       player.fame -= 50;
       localStorage.setItem('fame', player.fame);
-      localStorage.setItem('cosmetic', item);
-      alert(`Claimed ${item}!`);
+      document.getElementById('fame-value').innerText = player.fame;
+      player.cosmetic = item.toLowerCase().includes('hat') ? 'hat' : 'vest';
+      localStorage.setItem('cosmetic', player.cosmetic);
+      game.scene.scenes[0].playerSprite.setTexture(player.cosmetic);
+      alert(`Equipped ${item}!`);
     } else {
       alert('Need more Fame!');
     }
--- server.js
+++ server.js
@@ -3,9 +3,9 @@ const sqlite3 = require('sqlite3').verbose();
 const app = express();
 app.use(express.json());
 
-// Allow CORS for frontend
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 });
 
@@ -13,31 +13,36 @@ const db = new sqlite3.Database(':memory:');
 db.run('CREATE TABLE roster (id TEXT, beast TEXT)');
 
 async function generateBeast(biome) {
-    const adjectives = ['Fierce', 'Mystic', 'Shadow', 'Glimmer', 'Ancient'];
-    const nouns = ['Wyrm', 'Griffin', 'Serpent', 'Stag', 'Raven'];
-    const name = `${adjectives[Math.floor(Math.random() * adjectives.length)]} ${nouns[Math.floor(Math.random() * nouns.length)]}`;
-    const isShiny = Math.random() < 0.01;
-    const visuals = {
-      forest: 'https://opengameart.org/sites/default/files/monster_forest.png',
-      mountain: 'https://opengameart.org/sites/default/files/monster_mountain.png',
-      swamp: 'https://opengameart.org/sites/default/files/monster_swamp.png',
-      'shiny-forest': 'https://opengameart.org/sites/default/files/monster_forest_shiny.png',
-      'shiny-mountain': 'https://opengameart.org/sites/default/files/monster_mountain_shiny.png',
-      'shiny-swamp': 'https://opengameart.org/sites/default/files/monster_swamp_shiny.png'
-    };
-    const beast = {
-      name: `${name}_${Math.random().toString(36).slice(2, 7)}`, // Unique suffix
-      visual: visuals[isShiny ? `shiny-${biome}` : biome] || visuals[biome],
-      personality: ['Loyal', 'Mischievous', 'Stoic', 'Cunning'][Math.floor(Math.random() * 4)],
-      biome,
-      rarity: isShiny ? 'shiny' : (Math.random() < 0.1 ? 'rare' : 'common'),
-      traits: {
-        color: ['Red', 'Blue', 'Green', 'Black'][Math.floor(Math.random() * 4)],
-        size: ['Small', 'Medium', 'Large'][Math.floor(Math.random() * 3)]
-      }
-    };
-    return beast;
-  }
+  const adjectives = ['Fierce', 'Mystic', 'Shadow', 'Glimmer', 'Ancient'];
+  const nouns = ['Wyrm', 'Griffin', 'Serpent', 'Stag', 'Raven'];
+  const name = `${adjectives[Math.floor(Math.random() * adjectives.length)]} ${nouns[Math.floor(Math.random() * nouns.length)]}`;
+  const isShiny = Math.random() < 0.01;
+  const visuals = {
+    forest: 'https://opengameart.org/sites/default/files/monster_forest.png',
+    mountain: 'https://opengameart.org/sites/default/files/monster_mountain.png',
+    swamp: 'https://opengameart.org/sites/default/files/monster_swamp.png',
+    'shiny-forest': 'https://opengameart.org/sites/default/files/monster_forest_shiny.png',
+    'shiny-mountain': 'https://opengameart.org/sites/default/files/monster_mountain_shiny.png',
+    'shiny-swamp': 'https://opengameart.org/sites/default/files/monster_swamp_shiny.png'
+  };
+  const beast = {
+    name: `${name}_${Math.random().toString(36).slice(2, 7)}`,
+    visual: visuals[isShiny ? `shiny-${biome}` : biome] || visuals[biome],
+    personality: ['Loyal', 'Mischievous', 'Stoic', 'Cunning'][Math.floor(Math.random() * 4)],
+    biome,
+    rarity: isShiny ? 'shiny' : (Math.random() < 0.1 ? 'rare' : 'common'),
+    traits: {
+      color: ['Red', 'Blue', 'Green', 'Black'][Math.floor(Math.random() * 4)],
+      size: ['Small', 'Medium', 'Large'][Math.floor(Math.random() * 3)]
+    }
+  };
+  return beast;
+}
+
+app.get('/generate-beast', async (req, res) => {
+  const beast = await generateBeast(req.query.biome);
+  res.json(beast);
+});
 
 app.post('/save-roster', (req, res) => {
   const { playerId, beast } = req.body;
@@ -45,6 +50,14 @@ app.post('/save-roster', (req, res) => {
   res.sendStatus(200);
 });
 
-// Use Heroku's port or 3000 locally
+app.get('/get-roster', (req, res) => {
+  const { playerId } = req.query;
+  db.all('SELECT beast FROM roster WHERE id = ?', [playerId], (err, rows) => {
+    if (err) return res.status(500).send(err);
+    const roster = rows.map(row => JSON.parse(row.beast));
+    res.json(roster);
+  });
+});
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Server running on port ${port}`));
